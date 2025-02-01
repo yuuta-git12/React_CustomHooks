416810fa79845fd89a2e340a79a1f1c882c85fdf
yuuta-git12 <tayu2201@icloud.com>
2025-02-02 00:19:50 +0900

    [update]処理のカスタムフック化

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,13 @@ import axios from 'axios';
 import { User } from './types/api/user';
 import { useState } from 'react';
 import { UserProfile } from './types/userProfile';
+import { useAllUsers } from './hooks/useAllUsers';
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
 
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error} = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    axios
-      .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchUser = () => getUsers();
 
   return (
     <>
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { UserProfile } from "../types/userProfile";
+import { User } from "../types/api/user";
+import axios from "axios";
+
+//　全ユーザー一覧を取得するカスタムフック
+export const useAllUsers = () => {
+    const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const getUsers = () => {
+        setLoading(true);
+        setError(false);
+
+        axios
+        .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
+        .then((res) => {
+            const data = res.data.map((user) => ({
+            id: user.id,
+            name: `${user.name}(${user.username})`,
+            email: user.email,
+            address: `${user.address.city}${user.address.suite}${user.address.street}`,
+            }));
+            setUserProfiles(data);
+        })
+        .catch(() => {
+            setError(true);
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+    };
+    //　オブジェクトでgetUsersとステート情報を返す
+    return { getUsers, userProfiles, loading, error};
+};
